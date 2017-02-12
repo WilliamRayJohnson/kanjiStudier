b13546fb616332604d9378ccb3bfcda7040ec181
William Ray Johnson <willrayjohnson@gmail.com>
2017-02-12 15:37:57 -0600

    Changed test to print first 15 kanji, aka chapter 3 of Genki

--- readKanjiFileTest.py
+++ readKanjiFileTest.py
@@ -4,7 +4,7 @@ def main():
     testDict = readKanjiFile('kanjiTestFile.txt')
 
     print('There are {} entries in this dictionary.'.format(len(testDict)))
-    for entry in testDict:
+    for entry in list(testDict.items())[0:15]:
         print(entry)
 
 if __name__ == '__main__':
