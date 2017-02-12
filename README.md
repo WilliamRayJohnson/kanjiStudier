0de1d403be8b0ea1d0ed87d4df071b61ad0b5989
William Ray Johnson <willrayjohnson@gmail.com>
2017-02-12 01:41:02 -0600

    Added Genki Chp. 1 kanji to list, added README, and added skeleton of the main studier.

---
+++ README.md
@@ -0,0 +1,2 @@
+## The Kanji Studier
+A studier that will make studing kanji easier.
---
+++ kanjiStudier.py
@@ -0,0 +1,13 @@
+'''
+A program that will make studing kanji easier.
+William Ray Johnson
+'''
+
+from readKanjiFile.py import *
+
+def main():
+    return
+
+
+if __name__ == '__main__':
+    main()
