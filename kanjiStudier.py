0045caa63b21fe4597e52987ea5a940deb517350
William Ray Johnson <willrayjohnson@gmail.com>
2017-02-13 02:17:59 -0600

    Added setup for the study reading and writting functions to study a chapter.

--- kanjiStudier.py
+++ kanjiStudier.py
@@ -3,19 +3,35 @@ A program that will make studing kanji easier.
 William Ray Johnson
 '''
 
-from readKanjiFile.py import *
+from readKanjiFile import *
 
 KANJI_FILE = 'kanjiTestFile.txt'
+CHAP_RANGS = [(0,0), (0,0), (0,0), (0,15), (15,29), (29,43), (43,58), (58,72),
+        (72,86), (86,101), (101,115), (115,131), (131,145), (145,161),
+        (161,177), (177,193), (193,209)]
+
+def studyReading(studySet):
+    print("You are going to be reading these kanji: ")
+    for entry in studySet:
+        print(entry[0])
+
+def studyWritting(studySet):
+    print("You are going to be writting these kanji: ")
+    for entry in studySet:
+        print(entry[0])
 
 def main():
     kanjiDict = readKanjiFile(KANJI_FILE)
 
     print('There are {} kanji available to study.'.format(len(kanjiDict)))
+    chapter = input('Which chapter would you like to study? ')
+    chapRang = CHAP_RANGS[int(chapter)]
+    studySet = list(kanjiDict.items())[chapRang[0]:chapRang[1]]
     studyType = input('Would you like to study (r)eading or (w)riting?: ')
     if studyType == 'r':
-        studyReading()
+        studyReading(studySet)
     elif studyType == 'w':
-        studyWritting()
+        studyWritting(studySet)
 
 
 if __name__ == '__main__':
