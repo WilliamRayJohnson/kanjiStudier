5f4be495260c5c267de5f962fbb39f180c524fda
William Ray Johnson <willrayjohnson@gmail.com>
2017-02-16 15:45:36 -0600

    Implemented studyReading and studyWrittings function. Created the study loop enabling continuous study. Randomized studySet before being passed to study functions.

--- kanjiStudier.py
+++ kanjiStudier.py
@@ -4,6 +4,7 @@ William Ray Johnson
 '''
 
 from readKanjiFile import *
+from random import choice, shuffle
 
 KANJI_FILE = 'kanjiTestFile.txt'
 CHAP_RANGS = [(0,0), (0,0), (0,0), (0,15), (15,29), (29,43), (43,58), (58,72),
@@ -11,27 +12,37 @@ CHAP_RANGS = [(0,0), (0,0), (0,0), (0,15), (15,29), (29,43), (43,58), (58,72),
         (161,177), (177,193), (193,209)]
 
 def studyReading(studySet):
-    print("You are going to be reading these kanji: ")
     for entry in studySet:
-        print(entry[0])
+        word, reading = choice(list(entry[1].items()))
+        input(word)
+        input('It is read {}'.format(reading))
+        print()
 
 def studyWritting(studySet):
-    print("You are going to be writting these kanji: ")
     for entry in studySet:
-        print(entry[0])
+        word, reading = choice(list(entry[1].items()))
+        input(reading)
+        input('It is written {}'.format(word))
+        print()
 
 def main():
     kanjiDict = readKanjiFile(KANJI_FILE)
+    isStudying = True
 
-    print('There are {} kanji available to study.'.format(len(kanjiDict)))
-    chapter = input('Which chapter would you like to study? ')
-    chapRang = CHAP_RANGS[int(chapter)]
-    studySet = list(kanjiDict.items())[chapRang[0]:chapRang[1]]
-    studyType = input('Would you like to study (r)eading or (w)riting?: ')
-    if studyType == 'r':
-        studyReading(studySet)
-    elif studyType == 'w':
-        studyWritting(studySet)
+    while(isStudying):
+        print('There are {} kanji available to study.'.format(len(kanjiDict)))
+        chapter = input('Which chapter would you like to study? ')
+        chapRang = CHAP_RANGS[int(chapter)]
+        studySet = list(kanjiDict.items())[chapRang[0]:chapRang[1]]
+        shuffle(studySet)
+        studyType = input('Would you like to study (r)eading or (w)riting?: ')
+        if studyType == 'r':
+            studyReading(studySet)
+        elif studyType == 'w':
+            studyWritting(studySet)
+        willCont = input('Would you like to continue studying? (y)es (n)o: ')
+        if willCont[0] == 'n':
+            isStudying = False
 
 
 if __name__ == '__main__':
