c671301e29e41b0ea7988bad0a5ff4793890ea64
William Ray Johnson <willrayjohnson@gmail.com>
2017-02-20 21:39:34 -0600

    Test now prints out all kanji

--- readKanjiFileTest.py
+++ readKanjiFileTest.py
@@ -4,7 +4,7 @@ def main():
     testDict = readKanjiFile('kanjiTestFile.txt')
 
     print('There are {} entries in this dictionary.'.format(len(testDict)))
-    for entry in list(testDict.items())[0:15]:
+    for entry in list(testDict.items()):
         print(entry)
 
 if __name__ == '__main__':
