440a0c150b7c3f4986c1ff025e21c9628b1196be
William Ray Johnson <willrayjohnson@gmail.com>
2017-02-25 10:26:07 -0600

    Added option to study set of n random kanji

--- kanjiStudier.py
+++ kanjiStudier.py
@@ -4,7 +4,7 @@ William Ray Johnson
 '''
 
 from readKanjiFile import *
-from random import choice, shuffle
+from random import choice, shuffle, randint
 
 KANJI_FILE = 'genkiKanji.txt'
 CHAP_RANGS = [(0,0), (0,0), (0,0), (0,15), (15,29), (29,43), (43,58), (58,72),
@@ -25,23 +25,40 @@ def studyWritting(studySet):
         input('It is written {}'.format(word))
         print()
 
+def genRandKanjiSet(size, kanjiSet):
+    randKanjiSet = []
+
+    for kanjiNum in range(size):
+        randKanji = randint(0, len(kanjiSet))
+        while kanjiSet[randKanji] in randKanjiSet:
+            randKanji = randint(0, len(kanjiSet))
+        randKanjiSet.append(kanjiSet[randKanji])
+
+    return randKanjiSet
+
+
 def main():
     kanjiDict = readKanjiFile(KANJI_FILE)
     isStudying = True
 
     while(isStudying):
         print('There are {} kanji available to study.'.format(len(kanjiDict)))
-        chapter = input('Which chapter would you like to study? ')
-        chapRang = CHAP_RANGS[int(chapter)]
-        studySet = list(kanjiDict.items())[chapRang[0]:chapRang[1]]
-        shuffle(studySet)
+        studySetType = input("Would you like to study by (c)hapter or a (r)andom amount?")
+        if studySetType == 'c':
+            chapter = input('Which chapter would you like to study? ')
+            chapRang = CHAP_RANGS[int(chapter)]
+            studySet = list(kanjiDict.items())[chapRang[0]:chapRang[1]]
+            shuffle(studySet)
+        elif studySetType == 'r':
+            kanAmt = input("How many kanji would you like to study? ")
+            studySet = genRandKanjiSet(int(kanAmt), list(kanjiDict.items()))
         studyType = input('Would you like to study (r)eading or (w)riting?: ')
         if studyType == 'r':
             studyReading(studySet)
         elif studyType == 'w':
             studyWritting(studySet)
         willCont = input('Would you like to continue studying? (y)es (n)o: ')
-        if willCont[0] == 'n':
+        if willCont == 'n':
             isStudying = False
 
 
