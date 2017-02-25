14ad5179fc763ccb162d581fe9e98758feae4660
William Ray Johnson <willrayjohnson@gmail.com>
2017-02-25 01:17:10 -0600

    Moved print statement after for loop

--- readKanjiFileTest.py
+++ readKanjiFileTest.py
@@ -3,9 +3,9 @@ from readKanjiFile import *
 def main():
     testDict = readKanjiFile('genkiKanji.txt')
 
-    print('There are {} entries in this dictionary.'.format(len(testDict)))
     for entry in list(testDict.items()):
         print(entry)
+    print('There are {} entries in this dictionary.'.format(len(testDict)))
 
 if __name__ == '__main__':
     main()
